45bd2af3d009e80382ce4fe9d54b773f0e07b4c6
Patrigan <patrick.provinciael@gmail.com>
2021-06-14 21:41:14 +0200

    Working Water Flow Fix!

--- src/main/java/mod/patrigan/structure_toolkit/world/gen/processors/WaterflowFixProcessor.java
+++ src/main/java/mod/patrigan/structure_toolkit/world/gen/processors/WaterflowFixProcessor.java
@@ -2,18 +2,24 @@ package mod.patrigan.structure_toolkit.world.gen.processors;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.block.Blocks;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.IServerWorld;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.gen.feature.template.IStructureProcessorType;
 import net.minecraft.world.gen.feature.template.PlacementSettings;
 import net.minecraft.world.gen.feature.template.StructureProcessor;
 import net.minecraft.world.gen.feature.template.Template;
+import net.minecraft.world.server.ServerWorld;
 
 import static mod.patrigan.structure_toolkit.init.ModProcessors.WATERFLOW_FIX_PROCESSOR;
+import static net.minecraft.block.Blocks.WATER;
+import static net.minecraft.block.FlowingFluidBlock.LEVEL;
 
 public class WaterflowFixProcessor extends StructureProcessor {
 
@@ -23,21 +29,30 @@ public class WaterflowFixProcessor extends StructureProcessor {
     @Override
     public Template.BlockInfo process(IWorldReader world, BlockPos piecePos, BlockPos seedPos, Template.BlockInfo rawBlockInfo, Template.BlockInfo blockInfo, PlacementSettings settings, Template template) {
         // Workaround for water not flowing down when water has been placed earlier.
-        if(rawBlockInfo.pos.getX() == 0){
+        ServerWorld serverWorld = ((IServerWorld) world).getLevel();
+        if(blockInfo.state.is(WATER)) {
+            //serverWorld.getLiquidTicks().scheduleTick(blockInfo.pos, Fluids.WATER, Fluids.WATER.getTickDelay(serverWorld)*20);
+            /*ChunkPos currentChunkPos = new ChunkPos(blockInfo.pos);
+            IChunk currentChunk = world.getChunk(currentChunkPos.x, currentChunkPos.z);
+            currentChunk.setBlockState(blockInfo.pos, blockInfo.state, true);*/
+            return new Template.BlockInfo(blockInfo.pos, WATER.defaultBlockState().setValue(LEVEL, 7), null);
+            //((ServerWorld) world).setBlock(blockInfo.pos, blockInfo.state, 3);
+        }
+        /*if(rawBlockInfo.pos.getX() == 0){
             refreshFluid(world, blockInfo.pos, Direction.WEST);
         }else if(rawBlockInfo.pos.getX() == template.getSize().getX()){
             refreshFluid(world, blockInfo.pos, Direction.EAST);
         }
         if(rawBlockInfo.pos.getZ() == 0){
             refreshFluid(world, blockInfo.pos, Direction.NORTH);
-        }else if(rawBlockInfo.pos.getZ() == template.getSize().getZ()){
+        }else if(rawBlockInfo.pos.getZ() == template.getSize(BUI).getZ()){
             refreshFluid(world, blockInfo.pos, Direction.SOUTH);
         }
         if(rawBlockInfo.pos.getY() == 0){
             refreshFluid(world, blockInfo.pos, Direction.DOWN);
         }else if(rawBlockInfo.pos.getY() == template.getSize().getY()){
             refreshFluid(world, blockInfo.pos, Direction.UP);
-        }
+        }*/
         return blockInfo;
     }
 
